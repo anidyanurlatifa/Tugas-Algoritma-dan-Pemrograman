c51d1b7e68789decf912359218b65079fd5f8ada
anidyanurlatifa <113292614+anidyanurlatifa@users.noreply.github.com>
2023-03-28 13:16:13 +0700

    Tugas 2

--- main.cpp
+++ main.cpp
@@ -1,75 +1,21 @@
 #include <iostream>
-#include <cstdlib>
+
+#include <iostream>
 
 using namespace std;
 
-int tambah(int a, int b){
-    int c = a + b;
-    return c;
+bool isEven(int n) {
+  return n % 2 == 0;
 }
-
-int kurang(int a, int b){
-    int c = a - b;
-    return c;
+bool isOdd(int n) {
+  return n % 2 == 1;
 }
-
-int kali(int a, int b){
-    int c = a * b;
-    return c;
+int main() {
+  int x = 1007 ;
+  if (isEven(x)) {
+    std::cout << x << "  adalah Genap" << std::endl;
+  } else {
+    std::cout << x << "adalah Ganjil" << std::endl;
+  }
+  return 0;
 }
-
-float bagi(float a, int b){
-    float c = a / b;
-    return c;
-}
-
-int total(int arr[], int x){
-    int hasil = 0;
-    for(int i=0; i < x; i++){
-        hasil += arr[i];
-    }
-    return hasil;
-}
-
-
-float rata(int arr[],int x){
-    float hasil = 0;
-    for(int i=0; i < x; i++){
-        hasil += arr[i];
-    }
-    return hasil /= x;
-}
-
-
-int main()
-{
-    int x,y,n;
-    cout << "<----------------> Soal Tambah,Kurang,Kali,Bagi <--------------->"<< endl;
-    char h;
-    cout << "masukan angka(1): ";
-    cin >> x;
-    cout << "masukan angka(2): ";
-    cin >> y;
-    cout << "hasil: " << endl;
-
-    cout << "a.Ditambahkan = "<< tambah(x,y) << endl;
-    cout << "b.Dikurangkan = "<< kurang(x,y) << endl;
-    cout << "c.Dikalikan   = "<< kali(x,y) << endl;
-    cout << "d.Dibagikan   = "<< bagi(x,y) << endl;
-    cout <<  "<--------------------> Soal Element Array <------------------>" << endl;
-    cout << "Masukkan jumlah element Array  : ";
-    cin >> x;
-    int arr[x];
-    for(int i=0;i<x;i++){
-        cout << "Masukkan angka array ke : [" << i << "] = ";
-        cin >> arr [i];
-    }
-    cout <<  "<----------------------------------------------------------->" << endl;
-
-    cout << "Total hasil bilangan array\t\t: " << total(arr,x) << endl;
-    cout << "Rata - Rata hasil bilangan array\t\t: " << rata(arr,x) << endl;
-    cout <<  "<----------------------------------------------------------->" << endl;
-
-    return 0;
-}
-
